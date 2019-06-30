fc19a650c86f3a0d0b1e8668018be02c2045197d
Mahendran Mani <Mahendran.Mani@towerswatson.com>
2019-06-30 09:34:17 +0100

    included code sample to iterate over range using map

--- range.go
+++ range.go
@@ -3,4 +3,22 @@ package main
 import "fmt"
 
 func main(){
+   evens := []int{2,4,6,8,10}
+   alphabetFruitMap := map[string]string{ "a": "Apple", "b":"Banana", "c":"Citrus" }
+
+   for _, even := range evens{
+	fmt.Println( even)
+   }
+
+   for k,v := range alphabetFruitMap {
+	fmt.Printf("%s --> %s\n", k, v)
+   }
+
+   var stringIteration = "AazZ"
+
+   for c,u := range stringIteration{
+	fmt.Println(c ,u)
+
+   }
+   
 }
